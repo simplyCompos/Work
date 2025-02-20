7d58243371f5f4b5981e18a5a28fc7aa1e7cb948
Comos <you@example.com>
2025-02-20 11:39:35 +0200

    task9 finnale

---
+++ task9.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+void extract_message(const char* text) {
+	int state = 0;
+	int bit_index = 7;
+	char current_char = 0;
+	char extracted_message[100];
+	int j = 0;
+	int space_count = 0;
+	for (int i = 0; text[i] != '\0'; i++) {
+		if (text[i] == ' ') {
+			space_count++;
+		} else {
+			if (space_count > 0) {
+				int bit = (space_count == 2) ? 1 : 0;
+				current_char |= (bit << bit_index);
+				if (bit_index == 0) {
+					extracted_message[j++] = current_char;
+					current_char = 0;
+					bit_index = 7;
+				} else {
+					bit_index--;
+				}
+				space_count = 0;
+			}
+		}
+	}
+	extracted_message[j] = '\0';
+	std::cout << "Extracted message: " << extrated_message << std::endl;
+}
+int main(){
+	char text[500];
+	std::cout << "Enter text: ";
+	std::cin.getline(text, 500);
+	extracted_message(text);
+	return 0;
+}
