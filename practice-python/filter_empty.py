175470860f3ee5be7bd3a513da12dfa2653a90fe
Comos <you@example.com>
2025-06-09 13:48:09 +0300

    Filter update

--- practice-python/filter_empty.py
+++ practice-python/filter_empty.py
@@ -1,4 +1,6 @@
 import sys
-content = sys.stdin.read()
-filtered = "".join(filter(lambda ch: ch != "\n", content))
-print(filtered)
+lines = sys.stdin.readlines()
+filtered = [line for line in lines if line.strip() != ""]
+
+for line in filtered:
+    print(line, end="")
